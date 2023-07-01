5ff000e77874774faa38e09ea5eb3b8a99aadd19
kingkyle <38464469+kingkyle@users.noreply.github.com>
2023-07-01 03:24:07 +0100

    init constants for swagger

---
+++ app/internal/config/init_constants.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"os"
+
+	"github.com/kingsbloc/scissor/docs"
+)
+
+var (
+	HOST_URL      string
+	CONTACT_EMAIL = "kingsleynwankwou@gmail.com"
+	SCHEMES       []string
+)
+
+func InitConstants() {
+	if os.Getenv("APP_ENV") == "production" {
+		HOST_URL = "sms.herokuapp.com"
+		SCHEMES = []string{"https"}
+	} else {
+		HOST_URL = os.Getenv("SERVER_URL")
+		SCHEMES = []string{"http"}
+	}
+	docs.SwaggerInfo.Host = HOST_URL
+	docs.SwaggerInfo.Version = "0.1"
+	docs.SwaggerInfo.Title = "School Management System API"
+	docs.SwaggerInfo.Schemes = SCHEMES
+	docs.SwaggerInfo.BasePath = "/"
+}
